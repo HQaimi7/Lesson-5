4233eb8b83cfbd19b8fa2521601c8edbd0ca36da
HQaimi7 <Muhammad.Khan4@scchs.org.uk>
2024-10-14 20:18:27 +0100

    NestedLoops.py

---
+++ NestedLoops.py
@@ -0,0 +1,11 @@
+for i in range ( 1 , 6 ) : #row
+    for j in range ( 1 , 6 ) : #column
+        print(i, end=" ")
+    print("\r")
+
+print("-----------------------------------------------")
+
+for i in range ( 1 , 6 ) :
+    for j in range ( 1 , 6 ):
+        print(" 😎 ", end=" " )
+    print("\r")
